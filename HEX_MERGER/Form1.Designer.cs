21a14181d9eef658a716572a523e4f55ca8c8551
Parsa Malekpour <malekpour.parsa@Gmail.com>
2025-09-24 16:29:43 +0330

    init

---
+++ HEX_MERGER/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace HEX_MERGER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowesFile1 = new System.Windows.Forms.Button();
+            this.txtFile1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMainFile = new System.Windows.Forms.TextBox();
+            this.btnMainFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnShowBottloader = new System.Windows.Forms.Button();
+            this.btnShowMain = new System.Windows.Forms.Button();
+            this.lblSizeMain = new System.Windows.Forms.Label();
+            this.lblSizeBootloader = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnShowOutput = new System.Windows.Forms.Button();
+            this.lblSizeOutput = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.hexBox = new Be.Windows.Forms.HexBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowesFile1
+            // 
+            this.btnBrowesFile1.Location = new System.Drawing.Point(219, 18);
+            this.btnBrowesFile1.Name = "btnBrowesFile1";
+            this.btnBrowesFile1.Size = new System.Drawing.Size(52, 21);
+            this.btnBrowesFile1.TabIndex = 0;
+            this.btnBrowesFile1.Text = "Browes";
+            this.btnBrowesFile1.UseVisualStyleBackColor = true;
+            this.btnBrowesFile1.Click += new System.EventHandler(this.btnBrowesFile1_Click);
+            // 
+            // txtFile1
+            // 
+            this.txtFile1.Location = new System.Drawing.Point(54, 18);
+            this.txtFile1.Name = "txtFile1";
+            this.txtFile1.Size = new System.Drawing.Size(159, 20);
+            this.txtFile1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File 2:";
+            // 
+            // txtMainFile
+            // 
+            this.txtMainFile.Location = new System.Drawing.Point(54, 64);
+            this.txtMainFile.Name = "txtMainFile";
+            this.txtMainFile.Size = new System.Drawing.Size(159, 20);
+            this.txtMainFile.TabIndex = 4;
+            // 
+            // btnMainFile
+            // 
+            this.btnMainFile.Location = new System.Drawing.Point(219, 64);
+            this.btnMainFile.Name = "btnMainFile";
+            this.btnMainFile.Size = new System.Drawing.Size(52, 21);
+            this.btnMainFile.TabIndex = 3;
+            this.btnMainFile.Text = "Browes";
+            this.btnMainFile.UseVisualStyleBackColor = true;
+            this.btnMainFile.Click += new System.EventHandler(this.btnMainFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnShowBottloader);
+            this.groupBox1.Controls.Add(this.btnShowMain);
+            this.groupBox1.Controls.Add(this.lblSizeMain);
+            this.groupBox1.Controls.Add(this.lblSizeBootloader);
+            this.groupBox1.Controls.Add(this.txtFile1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnBrowesFile1);
+            this.groupBox1.Controls.Add(this.txtMainFile);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnMainFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 109);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inputs";
+            // 
+            // btnShowBottloader
+            // 
+            this.btnShowBottloader.Location = new System.Drawing.Point(277, 18);
+            this.btnShowBottloader.Name = "btnShowBottloader";
+            this.btnShowBottloader.Size = new System.Drawing.Size(28, 37);
+            this.btnShowBottloader.TabIndex = 9;
+            this.btnShowBottloader.Text = ">";
+            this.btnShowBottloader.UseVisualStyleBackColor = true;
+            this.btnShowBottloader.Click += new System.EventHandler(this.btnShowBottloader_Click);
+            // 
+            // btnShowMain
+            // 
+            this.btnShowMain.Location = new System.Drawing.Point(277, 64);
+            this.btnShowMain.Name = "btnShowMain";
+            this.btnShowMain.Size = new System.Drawing.Size(28, 37);
+            this.btnShowMain.TabIndex = 8;
+            this.btnShowMain.Text = ">";
+            this.btnShowMain.UseVisualStyleBackColor = true;
+            this.btnShowMain.Click += new System.EventHandler(this.btnShowMain_Click);
+            // 
+            // lblSizeMain
+            // 
+            this.lblSizeMain.AutoSize = true;
+            this.lblSizeMain.Location = new System.Drawing.Point(57, 87);
+            this.lblSizeMain.Name = "lblSizeMain";
+            this.lblSizeMain.Size = new System.Drawing.Size(46, 13);
+            this.lblSizeMain.TabIndex = 7;
+            this.lblSizeMain.Text = "             ";
+            // 
+            // lblSizeBootloader
+            // 
+            this.lblSizeBootloader.AutoSize = true;
+            this.lblSizeBootloader.Location = new System.Drawing.Point(57, 41);
+            this.lblSizeBootloader.Name = "lblSizeBootloader";
+            this.lblSizeBootloader.Size = new System.Drawing.Size(46, 13);
+            this.lblSizeBootloader.TabIndex = 6;
+            this.lblSizeBootloader.Text = "             ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnShowOutput);
+            this.groupBox2.Controls.Add(this.lblSizeOutput);
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Controls.Add(this.btnOutput);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 71);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // btnShowOutput
+            // 
+            this.btnShowOutput.Location = new System.Drawing.Point(277, 19);
+            this.btnShowOutput.Name = "btnShowOutput";
+            this.btnShowOutput.Size = new System.Drawing.Size(28, 37);
+            this.btnShowOutput.TabIndex = 9;
+            this.btnShowOutput.Text = ">";
+            this.btnShowOutput.UseVisualStyleBackColor = true;
+            this.btnShowOutput.Click += new System.EventHandler(this.btnShowOutput_Click);
+            // 
+            // lblSizeOutput
+            // 
+            this.lblSizeOutput.AutoSize = true;
+            this.lblSizeOutput.Location = new System.Drawing.Point(57, 42);
+            this.lblSizeOutput.Name = "lblSizeOutput";
+            this.lblSizeOutput.Size = new System.Drawing.Size(46, 13);
+            this.lblSizeOutput.TabIndex = 8;
+            this.lblSizeOutput.Text = "             ";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(54, 19);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(159, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(219, 19);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(52, 21);
+            this.btnOutput.TabIndex = 0;
+            this.btnOutput.Text = "Browes";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "File:";
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMerge.Image = global::HEX_MERGER.Properties.Resources._001_combine_data;
+            this.btnMerge.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMerge.Location = new System.Drawing.Point(12, 222);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(139, 48);
+            this.btnMerge.TabIndex = 8;
+            this.btnMerge.Text = "Merge   ";
+            this.btnMerge.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // hexBox
+            // 
+            this.hexBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexBox.LineInfoVisible = true;
+            this.hexBox.Location = new System.Drawing.Point(329, 12);
+            this.hexBox.Name = "hexBox";
+            this.hexBox.ReadOnly = true;
+            this.hexBox.ShadowSelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(60)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.hexBox.Size = new System.Drawing.Size(393, 305);
+            this.hexBox.StringViewVisible = true;
+            this.hexBox.TabIndex = 9;
+            this.hexBox.VScrollBarVisible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 329);
+            this.Controls.Add(this.hexBox);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Hex Merger";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowesFile1;
+        private System.Windows.Forms.TextBox txtFile1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMainFile;
+        private System.Windows.Forms.Button btnMainFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label lblSizeMain;
+        private System.Windows.Forms.Label lblSizeBootloader;
+        private System.Windows.Forms.Label lblSizeOutput;
+        private System.Windows.Forms.Button btnShowBottloader;
+        private System.Windows.Forms.Button btnShowMain;
+        private System.Windows.Forms.Button btnShowOutput;
+        private Be.Windows.Forms.HexBox hexBox;
+    }
+}
+
---
+++ HEX_MERGER/Form1.cs
@@ -0,0 +1,309 @@
+﻿using Be.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HEX_MERGER
+{
+    public partial class Form1 : Form
+    {
+
+        private string bootloaderPath = "";
+        private string mainPath = "";
+        private string outputPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnBrowesFile1_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Hex Files|*.hex";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    bootloaderPath = dlg.FileName;
+                    txtFile1.Text = bootloaderPath;
+                    var info = GetHexFileInfo(bootloaderPath);
+                    lblSizeBootloader.Text = $"Start: 0x{info.start:X8}, Size: {info.size} bytes";
+                }
+            }
+        }
+
+        private void btnMainFile_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Hex Files|*.hex";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    mainPath = dlg.FileName;
+                    txtMainFile.Text = mainPath;
+                    var info = GetHexFileInfo(mainPath);
+                    lblSizeMain.Text = $"Start: 0x{info.start:X8}, Size: {info.size} bytes";
+                }
+            }
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new SaveFileDialog())
+            {
+                dlg.Filter = "Hex Files|*.hex";
+                dlg.FileName = "combined.hex";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    outputPath = dlg.FileName;
+                    txtOutput.Text = outputPath;
+                    lblSizeOutput.Text = "Will be generated after merge";
+                }
+            }
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(bootloaderPath) || string.IsNullOrEmpty(mainPath) || string.IsNullOrEmpty(outputPath))
+            {
+                MessageBox.Show("Please select all files first.");
+                return;
+            }
+
+            try
+            {
+                var memory = new SortedDictionary<uint, byte>();
+                LoadHexFile(bootloaderPath, memory);
+                LoadHexFile(mainPath, memory);
+                WriteHexFile(outputPath, memory);
+
+                MessageBox.Show("Merge completed!\nFile saved at:\n" + outputPath);
+
+                var info = GetHexFileInfo(outputPath);
+                lblSizeOutput.Text = $"Start: 0x{info.start:X8}, Size: {info.size} bytes";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnShowBottloader_Click(object sender, EventArgs e)
+        {
+            if(btnShowBottloader.Text == "<")
+            {
+                hexBox.Visible = false;
+                btnShowBottloader.Text = ">";
+                this.Width = 350;
+            }
+
+            else if(File.Exists(bootloaderPath))
+            {
+                hexBox.LineInfoOffset = GetHexFileInfo(bootloaderPath).start;
+                LoadHexFile(bootloaderPath);
+                
+                hexBox.Visible = true;
+                btnShowBottloader.Text = "<";
+                this.Width = 750;
+            }
+            
+        }
+
+        private void btnShowMain_Click(object sender, EventArgs e)
+        {
+            if (btnShowMain.Text == "<")
+            {
+                hexBox.Visible = false;
+                btnShowMain.Text = ">";
+                this.Width = 350;
+            }
+
+            else if (File.Exists(mainPath))
+            {
+                hexBox.LineInfoOffset = GetHexFileInfo(mainPath).start;
+                LoadHexFile(mainPath);
+
+                hexBox.Visible = true;
+                btnShowMain.Text = "<";
+                this.Width = 750;
+            }
+        }
+
+        private void btnShowOutput_Click(object sender, EventArgs e)
+        {
+            if (btnShowOutput.Text == "<")
+            {
+                hexBox.Visible = false;
+                btnShowOutput.Text = ">";
+                this.Width = 350;
+            }
+
+            else if (File.Exists(outputPath))
+            {
+                hexBox.LineInfoOffset = GetHexFileInfo(outputPath).start;
+                LoadHexFile(outputPath);
+
+                hexBox.Visible = true;
+                btnShowOutput.Text = "<";
+                this.Width = 750;
+            }
+        }
+
+        // ===================== HEX LOGIC =====================
+        private void LoadHexFile(string filePath)
+        {
+            // Read the whole file into memory (file will be closed immediately)
+            byte[] data = File.ReadAllBytes(filePath);
+
+            // Use DynamicByteProvider (in-memory, editable)
+            var byteProvider = new DynamicByteProvider(data);
+            hexBox.ByteProvider = byteProvider;
+        }
+
+
+        private (uint start, uint size) GetHexFileInfo(string path)
+        {
+            uint extAddr = 0;
+            uint? minAddr = null;
+            uint maxAddr = 0;
+
+            foreach (var line in File.ReadAllLines(path))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line[0] != ':') continue;
+
+                int length = Convert.ToInt32(line.Substring(1, 2), 16);
+                uint addr = Convert.ToUInt32(line.Substring(3, 4), 16);
+                int type = Convert.ToInt32(line.Substring(7, 2), 16);
+
+                if (type == 0x00) // data
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        uint fullAddr = extAddr + addr + (uint)i;
+                        if (minAddr == null || fullAddr < minAddr) minAddr = fullAddr;
+                        if (fullAddr > maxAddr) maxAddr = fullAddr;
+                    }
+                }
+                else if (type == 0x04) // extended linear address
+                {
+                    extAddr = (uint)Convert.ToUInt32(line.Substring(9, 4), 16) << 16;
+                }
+            }
+
+            uint start = minAddr ?? 0;
+            uint size = (minAddr == null) ? 0 : (maxAddr - start + 1);
+            return (start, size);
+        }
+
+        private void LoadHexFile(string path, SortedDictionary<uint, byte> memory)
+        {
+            uint extAddr = 0;
+            foreach (var line in File.ReadAllLines(path))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line[0] != ':') continue;
+
+                int length = Convert.ToInt32(line.Substring(1, 2), 16);
+                uint addr = Convert.ToUInt32(line.Substring(3, 4), 16);
+                int type = Convert.ToInt32(line.Substring(7, 2), 16);
+
+                if (type == 0x00) // Data record
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        byte data = Convert.ToByte(line.Substring(9 + i * 2, 2), 16);
+                        uint fullAddr = extAddr + addr + (uint)i;
+                        memory[fullAddr] = data;
+                    }
+                }
+                else if (type == 0x04) // Extended Linear Address
+                {
+                    extAddr = (uint)Convert.ToUInt32(line.Substring(9, 4), 16) << 16;
+                }
+            }
+        }
+
+        private void WriteHexFile(string path, SortedDictionary<uint, byte> memory)
+        {
+            using (var writer = new StreamWriter(path))
+            {
+                uint? currentSegment = null;
+                List<byte> buffer = new List<byte>();
+                ushort lastOffset = 0;
+                uint lastAddr = 0;
+
+                foreach (var kvp in memory)
+                {
+                    uint addr = kvp.Key;
+                    byte data = kvp.Value;
+
+                    uint segment = addr >> 16;
+                    ushort offset = (ushort)(addr & 0xFFFF);
+
+                    if (currentSegment != segment)
+                    {
+                        if (buffer.Count > 0)
+                        {
+                            WriteDataRecord(writer, lastOffset, buffer);
+                            buffer.Clear();
+                        }
+                        // Extended Linear Address record
+                        string rec = $":02000004{segment:X4}{Checksum(2, 0, 4, (byte)(segment >> 8), (byte)(segment & 0xFF)):X2}";
+                        writer.WriteLine(rec);
+                        currentSegment = segment;
+                    }
+
+                    if (buffer.Count == 0)
+                        lastOffset = offset;
+                    else if (offset != lastOffset + buffer.Count || buffer.Count >= 16)
+                    {
+                        WriteDataRecord(writer, lastOffset, buffer);
+                        buffer.Clear();
+                        lastOffset = offset;
+                    }
+
+                    buffer.Add(data);
+                    lastAddr = addr;
+                }
+
+                if (buffer.Count > 0)
+                    WriteDataRecord(writer, lastOffset, buffer);
+
+                writer.WriteLine(":00000001FF"); // EOF
+            }
+        }
+
+        private void WriteDataRecord(StreamWriter writer, ushort offset, List<byte> data)
+        {
+            int len = data.Count;
+            int sum = len + (offset >> 8) + (offset & 0xFF) + 0x00;
+            string record = $":{len:X2}{offset:X4}00";
+
+            foreach (var b in data)
+            {
+                record += $"{b:X2}";
+                sum += b;
+            }
+
+            byte chk = (byte)((~sum + 1) & 0xFF);
+            record += $"{chk:X2}";
+            writer.WriteLine(record);
+        }
+
+        private byte Checksum(params int[] bytes)
+        {
+            int sum = 0;
+            foreach (int b in bytes) sum += b;
+            return (byte)((~sum + 1) & 0xFF);
+        }
+
+ 
+    }
+}
